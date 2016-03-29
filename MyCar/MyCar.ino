51d9c452879f9c58716f1d944d822a893fbe7682
risoriuz <alex.panacea@yandex.com>
2016-03-30 00:16:03 +0300

    Добавил работу програмного serial

--- MyCar/MyCar.ino
+++ MyCar/MyCar.ino
@@ -3,59 +3,93 @@
 // this code is public domain, enjoy!
 
 #include <AFMotor.h>
-const int TopSpeed = 127;
+#include <SoftwareSerial.h>
 
 AF_DCMotor motorM1(1);  //Задний мост
 AF_DCMotor motorM3(1);  //Руль
+SoftwareSerial BTSerial(A0, A1); // RX, TX
+
+int maxSpeed = 127;   //Ограничение максимальной скорости
+char btcmd;           // Переменнвя для комманд BT
+int vpsdM1;           //Память скорости M1
 
 void setup() {
   Serial.begin(9600);              // set up Serial library at 9600 bps
-  Serial.println("Motor test!");
+  BTSerial.begin(9600);
 
-  // turn on motor
   motorM1.setSpeed(100);   //speed- Valid values for 'speed' are between 0 and 255 with 0 being off and 255 as full throttle.
   motorM1.run(RELEASE);
-  
+
   motorM3.setSpeed(100);
   motorM3.run(RELEASE);
-  
 }
 
 void loop() {
   uint8_t i;
-  
-  Serial.print("Задний мост вперед: ");
-  
-  motorM1.run(FORWARD);
-  for (i=0; i<TopSpeed; i++) {
-    motorM1.setSpeed(i);  
-    delay(10);
- }
-  Serial.print(" Полная скорость =");
-  Serial.println(TopSpeed);
-  for (i=TopSpeed; i!=0; i--) {
-    motorM1.setSpeed(i);  
-    delay(100);
- }
-  
-  Serial.println("Задний мост вперед: СТОП");
-  delay (1000);
-  Serial.println("Ждем 1000 ms");
+  if (BTSerial.available())
+  {
+    btcmd = (char)BTSerial.read();
+    Serial.println(btcmd);
 
-  Serial.print("Задний мост назад: ");
-  motorM1.run(BACKWARD);
-  for (i=0; i<TopSpeed; i++) {
-    motorM1.setSpeed(i);  
-    delay(100);
- }
- 
-  for (i=TopSpeed; i!=0; i--) {
-    motorM1.setSpeed(i);  
-    delay(100);
- }
-  Serial.println("Задний мост назад: СТОП");
+    // Вперед
+    if (btcmd == 'F') {
+      motorM1.run(FORWARD);
+      for (i = 0; i < vpsdM1; i++) {
+        motorM1.setSpeed(i);
+        delay(10);
+      }
+    }
+    
+    // Назад
+    if (btcmd == 'B') {
+      for (i = 0; i < vpsdM1; i++) {
+        motorM1.setSpeed(i);
+        delay(10);
+      }
+    }
+  }
 
-  Serial.println("Вылючение");
-  motorM1.run(RELEASE);
-  delay(1000);
+
+
 }
+// Прямо влево
+void fLeft() {
+  Speed(vpsdM1,maxSpeed);
+  motorM1.run(FORWARD);
+  motorM3.run(FORWARD);
+}
+
+// Прямо вправо
+void fRight() {
+  Speed(vpsdM1,maxSpeed);
+  motorM1.run(FORWARD);
+  motorM3.run(BACKWARD);
+}
+
+// Назад влево
+void bLeft() {
+  Speed(vpsdM1,maxSpeed);
+  motorM1.run(BACKWARD);
+  motorM3.run(FORWARD);
+}
+
+// Назад направо
+void bRight() {
+  Speed(vpsdM1,maxSpeed);
+  motorM1.run(BACKWARD);
+  motorM3.run(BACKWARD);
+}
+// Изменение скорости
+void Speed(int spd, int max) {
+  if (spd > max) {
+    spd = max;
+  }
+  motorM1.setSpeed(spd);
+}
+
+//Остановка моторов
+void MotorsStop() {
+    motorM1.run(RELEASE);
+    motorM3.run(RELEASE);
+}
+
